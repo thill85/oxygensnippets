ac0314c6db5a09ba9474d63d489718e6b44a36c0
Tim <60788504+thill85@users.noreply.github.com>
2022-03-17 09:19:41 -0500

    Create remove-archive-label.php

---
+++ remove-archive-label.php
@@ -0,0 +1,27 @@
+
+/**
+ * Remove archive labels.
+ * 
+ * @param  string $title Current archive title to be displayed.
+ * @return string        Modified archive title to be displayed.
+ */
+
+add_filter( 'get_the_archive_title', 'my_theme_archive_title' );
+ 
+function my_theme_archive_title( $title ) {
+	if ( is_category() ) {
+		$title = single_cat_title( '', false );
+	} elseif ( is_tag() ) {
+		$title = single_tag_title( '', false );
+	} elseif ( is_author() ) {
+		$title = '<span class="vcard">' . get_the_author() . '</span>';
+	} elseif ( is_post_type_archive() ) {
+		$title = post_type_archive_title( '', false );
+	} elseif ( is_tax() ) {
+		$title = single_term_title( '', false );
+	} elseif ( is_home() ) {
+		$title = single_post_title( '', false );
+	}
+
+	return $title;
+}
